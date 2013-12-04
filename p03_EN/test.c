da45db19dc7ab149b4c56b121a9c06d33d8d61c8
Mario <mario@Marios-Mac-Pro.local>
2013-12-04 19:34:00 +0100

    made some tests on system calls

--- p03_EN/test.c
+++ p03_EN/test.c
@@ -2,12 +2,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <sys/uio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main()
 {
 	char* _exit;
-	int fd = open("testfile",O_RDWR);
-	printf("%d\n",fd );
+	char* cinbuffer;
+	int _fd = open("testfile",O_RDWR);
+	int _sizeFile = read(_fd,&cinbuffer,5);
+	int _bytesW = write(_fd,&cinbuffer,sizeof(cinbuffer));
+	printf("%d\n",_sizeFile );
+	printf("%s\n", cinbuffer);
 	scanf("%s",_exit);
 	return 0;
 }
