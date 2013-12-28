152a6047a168c6781eedef5d0b3fd3f72c883cef
Mario <mario@Marios-Mac-Pro.local>
2013-12-28 19:26:13 +0100

    still don't know what the f**k shall i do

--- p04_EN/pipes/main.c
+++ p04_EN/pipes/main.c
@@ -12,10 +12,10 @@ int main()
 	{
 		case -1: 	perror("fork");  	//error
 					return -2;
-		case 0: 	close(forth[1]);	//child
+		case 0: 	close(forth[1]);	//IO mux process (child)
 					close(back[0]);
 					return io(forth[0], back[1]);
-		default: 	close(forth[0]);
+		default: 	close(forth[0]);	//main process
 					close(back[1]);
 					r = caps(back[0],forth[1]);
 					wait(NULL);
---
+++ p04_EN/pipes/str_hdl.h
@@ -0,0 +1,9 @@
+#ifndef SYMBOL
+#define SYMBOL value
+#define SIZE 100
+#define SIZE_HALF ((SIZE)/2 +1)
+
+void concatenate(char all[SIZE], char half1[SIZE_HALF], char half2[SIZE_HALF]);
+void split(char all[SIZE], char half1[SIZE_HALF], char half2[SIZE_HALF]);
+
+#endif
