b62944b2aea903be6d41e4b720b3620846cdca76
Mario <mario@Marios-Mac-Pro.local>
2014-01-02 20:34:58 +0100

    finished demux process

--- p04_EN/pipes/demux.c
+++ p04_EN/pipes/demux.c
@@ -1,5 +1,34 @@
 int demux(int readend1, int readend2, int writeend)
 {
-	printf("%s\n", demux);
-	return 1;
+	int r1,r2;
+	char buff1[SIZE_HALF];
+	char buff2[SIZE_HALF];
+	char all[SIZE];
+
+	for (;;)
+	{
+		r1 = read(readend1, buff1, SIZE_HALF);
+
+		if (r1 < 0)
+			break;
+
+		r2 = read(readend2, buff2, SIZE_HALF);
+
+		if (r2 < 0)
+			break;
+
+		if ((r1 == 0) && (r2 == 0))
+		{
+			close(writeend);
+			return 0;
+		}
+
+		concatenate(all,buff1,buff2);
+
+		if (write(writeend, all, 1+strlen(all)) < 0)
+			break;
+	}
+
+	perror("demux");
+	return -1;
 }
